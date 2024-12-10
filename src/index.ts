75b0a84dfe6a729d9806012e74da29ef77ceb12e
Valeriya Kroshnina <flora.valeriya@mail.ru>
2024-12-10 16:41:51 +0500

    Сделана адаптивная верстка

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+const news = document.getElementById('news');
+
+type TNews = {
+    title: string;
+    author: string;
+    description: string;
+    urlToImage: string;
+    sourse: TSourse;
+};
+
+type TSourse = {
+    id: string;
+    name: string;
+};
+
+const apiUrl = 'https://newsapi.org/v2/';
+const apiKey = 'dc9ca8f058e14a779f7e3bc7b66ef592';
+
+async function fetchNews() {
+    const response = await fetch(`${apiUrl}everything?q=tesla&from=2024-11-10&sortBy=publishedAt&apiKey=${apiKey}`);
+    const obj = await response.json();
+    console.log(obj.articles);
+    renderNews(obj.articles);
+}
+
+fetchNews();
+
+function renderNews(newsArr: TNews[]) {
+    newsArr.forEach((newItem) => {
+        const listItem = document.createElement('div');
+        listItem.className = 'news__item';
+        news?.appendChild(listItem);
+
+        const imgItem = document.createElement('img');
+        imgItem.src = newItem.urlToImage;
+        listItem.appendChild(imgItem);
+
+        const titleItem = document.createElement('h2');
+        titleItem.innerHTML = newItem.title;
+        listItem.appendChild(titleItem);
+
+        const descriptionItem = document.createElement('p');
+        descriptionItem.innerHTML = newItem.description;
+        listItem.appendChild(descriptionItem);
+
+        const authorItem = document.createElement('h3');
+        authorItem.innerHTML = newItem.author;
+        listItem.appendChild(authorItem);
+    });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,23 @@
 const path = require('path');
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const DotenvWebpackPlugin = require('dotenv-webpack');
 
-const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
-    mode: 'development',
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
             },
         ],
     },
-    resolve: {
-        extensions: ['.js'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    plugins: [
-        new DotenvWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
-        new CleanWebpackPlugin(),
-    ],
-};
-
-module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
-
-    return merge(baseConfig, envConfig);
+    mode: 'development',
 };
